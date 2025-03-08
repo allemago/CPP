2f34982e151900f65ba04b24437c6473b3489f3e
MAELLE <gbk.maelle@outlook.fr>
2025-03-08 23:58:52 +0100

    CPP01 done

--- CPP01/ex06/main.cpp
+++ CPP01/ex06/main.cpp
@@ -6,7 +6,7 @@
 /*   By: magrabko <magrabko@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/08 22:28:29 by magrabko          #+#    #+#             */
-/*   Updated: 2025/03/08 23:18:05 by magrabko         ###   ########.fr       */
+/*   Updated: 2025/03/08 23:58:04 by magrabko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,7 @@
 int main(int argc, char **argv)
 {
 	if (argc != 2)
-	{
-		std::cerr << ARG_MSG << std::endl;
-		return (1);
-	}
+		return (std::cerr << ARG_MSG << std::endl, 1);
 	Harl complainer(argv[1]);
 	complainer.complain(argv[1]);
 	return (0);
