68fb56e87a56ec22d37ca17538c3a210bb641f57
MAELLE <gbk.maelle@outlook.fr>
2025-05-21 20:39:30 +0200

    CPP05 -> ex00 done

--- CPP05/ex00/src/main.cpp
+++ CPP05/ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: magrabko <magrabko@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/15 17:02:08 by magrabko          #+#    #+#             */
-/*   Updated: 2025/05/15 19:32:07 by magrabko         ###   ########.fr       */
+/*   Updated: 2025/05/21 20:38:23 by magrabko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,13 @@ int	main()
 		try
 		{
 			Bureaucrat a("a", 15);
-			std::cout << a.getName() << " grade is " << a << std::endl;
+			std::cout << a.getName() << ", bureaucrat grade " << a << "." << std::endl;
 			Bureaucrat b("b", 1);
-			std::cout << b.getName() << " grade is " << b << std::endl;
-			Bureaucrat c("c", 155);
-			std::cout << c.getName() << " grade is " << c << std::endl;
+			std::cout << b.getName() << ", bureaucrat grade " << b << "." << std::endl;
+			Bureaucrat c("c", 151);
+			std::cout << c.getName() << ", bureaucrat grade " << c << "." << std::endl;
 			Bureaucrat d("d", 0);
-			std::cout << d.getName() << " grade is " << d << std::endl;
+			std::cout << d.getName() << ", bureaucrat grade " << d << "." << std::endl;
 		}
 		catch (std::exception& e)
 		{
@@ -37,14 +37,30 @@ int	main()
 		std::cout << GREEN "TEST 2" RESET << std::endl;
 		try
 		{
-			Bureaucrat a("a", 1);
-			std::cout << a.getName() << " grade is " << a << std::endl;
-			for (int i = 0; i < 9; i++)
-				a.decrementGrade();
-			std::cout << a.getName() << " grade is " << a << std::endl;
-			for (int i = 0; i < 10; i++)
+			Bureaucrat a("a", 150);
+			std::cout << a.getName() << ", bureaucrat grade " << a << "." << std::endl;
+			for (int i = 0; i < 149; i++)
 				a.incrementGrade();
-			std::cout << a.getName() << " grade is " << a << std::endl;
+			std::cout << a.getName() << ", bureaucrat grade " << a << "." << std::endl;
+			for (int i = 0; i < 1; i++)
+				a.incrementGrade();
+			std::cout << a.getName() << ", bureaucrat grade " << a << "." << std::endl;
+		}
+		catch (std::exception& e)
+		{
+			std::cout << e.what() << std::endl;
+		}
+		std::cout << std::endl;
+	}
+	{	
+		std::cout << GREEN "TEST 3" RESET << std::endl;
+		try
+		{
+			Bureaucrat a("a", 1);
+			std::cout << a.getName() << ", bureaucrat grade " << a << "." << std::endl;
+			for (int i = 0; i < 149; i++)
+				a.decrementGrade();
+			std::cout << a.getName() << ", bureaucrat grade " << a << "." << std::endl;
 		}
 		catch (std::exception& e)
 		{
