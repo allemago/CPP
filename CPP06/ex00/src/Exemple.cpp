ce35ae13c1cce3889597c7d4664df83e7cd5b738
Maelle Grabko <magrabko@f6r4s13.clusters.42paris.fr>
2025-05-29 15:57:08 +0200

    ajout CPP06

---
+++ CPP06/ex00/src/Exemple.cpp
@@ -0,0 +1,10 @@
+#include "../include/Exemple.hpp"
+
+Exemple::Exemple() {}
+
+Exemple::Exemple(const Exemple& object)
+{
+	*this = object;
+}
+
+Exemple::~Exemple() {}
---
+++ CPP06/ex01/include/Exemple.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+# include <iostream>
+
+class Exemple
+{
+/*****************
+*    PRIVATE     *
+*****************/
+private:
+
+/*****************
+*     PUBLIC     *
+*****************/
+public:
+//	==================== Canonical Form =========================
+
+	Exemple();
+	Exemple();
+	~Exemple();
+	Exemple&	operator=(const Exemple&);
+
+//	==================== Custom Constructors ====================
+//	==================== Getters / Setters ======================
+//	==================== Public Methods =========================
+//	==================== Exceptions =============================
+};
